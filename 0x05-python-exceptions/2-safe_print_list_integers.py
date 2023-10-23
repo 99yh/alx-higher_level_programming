d77d2420d081dcd1aa4aedaed11e1a2b2ff51768
Yahia Soliman <yn7599@gmail.com>
2023-10-23 15:28:29 +0300

    Mysterious Errors 3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            n += 1
-        except (ValueError, TypeError):
-            continue
-        except IndexError:
-            break
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                n += 1
+            except (ValueError, TypeError):
+                continue
+            except IndexError:
+                break
+    except TypeError:
+        pass
     print()
     return n
