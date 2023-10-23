cc54f3a45503d6f217621eeaa93f60f59d066db2
Yahia Soliman <yn7599@gmail.com>
2023-10-23 15:12:57 +0300

    Mysterious Errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             n += 1
         except (ValueError, TypeError):
             continue
+        except IndexError:
+            break
     n and print()
     return n
