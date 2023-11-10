f126854bd93fee938c5caf4fd8c84415545dec28
Yahia <yn7599@gmail.com>
2023-11-10 16:29:39 +0200

    file i/o finish

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,7 +5,7 @@
 def class_to_json(obj):
     """Get a dictionary of simple data structures related to an object."""
     json_dict = {}
-    obj_dict = obj.__dict__ #| obj.__class__.__dict__
+    obj_dict = obj.__dict__
     simple_types = [list, dict, str, int, float, bool]
     for k, v in obj_dict.items():
         if type(v) in simple_types:
