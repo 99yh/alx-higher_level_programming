fd7cce3dcdffde06d9535e5fe4bf638c61fe69df
Yahia <yn7599@gmail.com>
2023-12-15 01:24:10 +0200

    SQL intro

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- usage: cat 4-first_table.sql | mysql -hlocalhost -uroot -p <database name>
+-- create a new table
+SELECT score, name FROM second_table
+ ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- usage: cat 4-first_table.sql | mysql -hlocalhost -uroot -p <database name>
+-- create a new table
+SELECT COUNT(id) FROM first_table WHERE id = 89;
