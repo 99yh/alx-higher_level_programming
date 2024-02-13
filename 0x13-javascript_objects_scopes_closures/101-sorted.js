99b4016dfcda8b27b9efe086b6d738bacf6aced1
Yahia <yn7599@gmail.com>
2024-02-13 21:47:38 +0200

    do you need try catch?

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-const newDict = {};
-
-for (const prop in dict) {
-  newDict[dict[prop]] ||= [];
-  newDict[dict[prop]].push(prop);
-}
-if (Object.keys(newDict).length) console.log(newDict);
+try {
+  const dict = require('./101-data').dict;
+  const newDict = {};
+  for (const prop in dict) {
+    newDict[dict[prop]] ||= [];
+    newDict[dict[prop]].push(prop);
+  }
+  console.log(newDict);
+} catch {}
