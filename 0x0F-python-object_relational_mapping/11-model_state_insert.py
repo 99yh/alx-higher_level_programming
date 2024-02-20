cfc1a116423e92c517686a41b12124f4c2b98590
Yahia <yn7599@gmail.com>
2024-02-20 22:40:18 +0200

    tried ALL except relations

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
-
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -12,11 +11,12 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3]
         ), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
-
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -12,11 +11,11 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3]
         ), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).order_by(State.id).first()
+    session = sessionmaker(bind=engine)()
+    state = session.query(State).filter(State.id == 2).first()
+
     if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
+        state.name = "New Mexico"
+    session.commit()
