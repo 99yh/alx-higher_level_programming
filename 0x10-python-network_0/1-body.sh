9d181b63d7f160e3da9cb68c1a53fda1b9b561ba
Yahia <yn7599@gmail.com>
2024-02-29 20:05:55 +0200

    200 OK!

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # GET the body of a response if only the status code is 200
-curl -sI "$1" | grep -q '200 OK' && curl -s "$1"
+[ "$(curl -sI -w '%{http_code}' "$1" | tail -n 1)" == 200 ] && curl -s "$1"
