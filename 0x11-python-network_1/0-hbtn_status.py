d5b6160f035906e6e980adc716004a5b8536f0b6
Yahia <yn7599@gmail.com>
2024-03-01 20:38:45 +0200

    if name == 'main': import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """Fetch alx /status to know how it feels"""
 from urllib.request import urlopen
 
-
-with urlopen('https://alx-intranet.hbtn.io/status') as res:
-    body = res.read()
-    infos = 'Body response:'\
-            '\n\t- type: {}'\
-            '\n\t- content: {}'\
-            '\n\t- utf8 content: {}'
-    print(infos.format(type(body), body, body.decode('utf8')))
+if __name__ == '__main__':
+    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+        infos = 'Body response:'\
+                '\n\t- type: {}'\
+                '\n\t- content: {}'\
+                '\n\t- utf8 content: {}'
+        print(infos.format(type(body), body, body.decode('utf8')))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,8 @@ import urllib.parse
 import sys
 
 
-data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as res:
-    print(res.read().decode('utf8'))
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf8'))
