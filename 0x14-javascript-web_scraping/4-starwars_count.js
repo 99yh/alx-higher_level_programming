f10bc0d7c6237300aa1f8715317fabba829edbf2
Yahia <yn7599@gmail.com>
2024-03-26 15:45:30 +0000

    ok it's an ARGUMENT

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2] || 'https://swapi-api.alx-tools.com/api/films/';
+const url = process.argv[2];
 const lookfor = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-request(url + '?format=json', function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) return console.error(error);
   const all = JSON.parse(body);
   const needed = all.results?.filter((film) => film.characters?.includes(lookfor));
