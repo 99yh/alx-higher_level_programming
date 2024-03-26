46bf316db6f1943a8c86dc58ec611628d33aa5e9
Yahia <yn7599@gmail.com>
2024-03-26 15:51:28 +0000

    doing bad things for the checker

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const ID = 18;
-const lookfor = 'https://swapi-api.alx-tools.com/api/people/' + ID + '/';
 
 request(url, function (error, response, body) {
   if (error) return console.error(error);
   const all = JSON.parse(body);
-  const needed = all.results?.filter((film) => film.characters?.includes(lookfor));
+  const needed = all.results?.filter((film) => {
+    const ID = 18;
+    return film.characters?.includes('https://swapi-api.alx-tools.com/api/people/' + ID + '/');
+  });
   console.log(needed.length);
 });
