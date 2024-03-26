a098e77e29612a85b277866bf801611890dd2780
Yahia <yn7599@gmail.com>
2024-03-26 16:07:50 +0000

    finally mandatory

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) return console.error(error);
+  const data = JSON.parse(body);
+  const result = {};
+  for (const item of data) {
+    if (result[item.userId]) result[item.userId]++;
+    else if (item.userId) result[item.userId] = 1;
+  }
+  console.log(result);
+});
